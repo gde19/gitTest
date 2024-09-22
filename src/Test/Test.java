7c75f4f1e351ec56fe322414931d66eb3a8c4722
Danila Glukhov <Hock63D@gmail.com>
2024-09-22 12:30:27 +0400

    Adding new class

---
+++ src/Test/Test.java
@@ -0,0 +1,25 @@
+package Test;
+
+public class Test {
+    private int id;
+
+    public Test(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public String toString() {
+        return "Test{" +
+                "id=" + id +
+                '}';
+    }
+}
+
